49e0daa2328b75e4239c9b140af25fa7e5876a17
Kevin Johel Bilarcs Sub Coy <ksub@FEDECOVERA.CORP>
2026-03-11 11:51:44 -0600

    Actualización de ruta de información para backend desplegado

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Rutas
-app.get('/', (req, res) => {
+app.get('/api/health', (req, res) => {
   res.json({
     success: true,
     message: 'API de Sistema de Inscripción a Eventos',
